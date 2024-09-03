06f5cf9771467494c5cf71c355a0f157e39a609e
Pao - Wasut Panyawiphat <wasut_pao@hotmail.com>
2024-09-03 18:45:07 +0700

    add tailwind theme and config

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -2,7 +2,27 @@
 export default {
 	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
 	theme: {
-		extend: {},
+		extend: {
+			colors: {
+				primary: "var(--color-primary)",
+				secondary: "var(--color-secondary)",
+			},
+			textColor: {
+				default: "var(--color-text)",
+				offset: "var(--color-text-offset)",
+			},
+			backgroundColor: {
+				default: "var(--color-background)",
+				offset: "var(--color-background-offset)",
+			},
+			borderColor: {
+				default: "var(--color-border)",
+			},
+			animation: {
+				"spin-slower": "spin 35s ease infinite",
+				"spin-slow": "spin 25s ease-in-out infinite reverse",
+			},
+		}
 	},
 	plugins: [],
 }
